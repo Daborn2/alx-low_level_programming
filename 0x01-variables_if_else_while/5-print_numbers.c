2285604195ba3d472d5679ae42601eab1627419c
Daborn1 <orjimiracle870@gmail.com>
2022-12-09 17:51:30 -0800

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * x: variable
+ * Return: return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		printf("%d", x);
+	printf("\n");
+
+	return (0);
+}
